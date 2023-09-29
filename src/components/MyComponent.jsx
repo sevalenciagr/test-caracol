9dedbdb620d405cf8bb2496873a0d3a5ac9f6858
Sebastián Valencia G <sevalenciagr@gmail.com>
2023-09-29 12:09:56 -0500

    Adding Styles

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,3 +1,125 @@
+// import React, { useState, useEffect } from 'react';
+// import Handlebars from 'handlebars';
+// import { fetchData } from './api';
+
+// const MyComponent = () => {
+//   const [jsonData, setJsonData] = useState(null);
+
+//   useEffect(() => {
+//     const fetchDataFromApi = async () => {
+//       try {
+//         const data = await fetchData('/public/test.json');
+//         setJsonData(data);
+//       } catch (error) {
+//         console.error('Error:', error);
+//       }
+//     };
+
+//     fetchDataFromApi();
+//   }, []);
+
+//   if (!jsonData) {
+//     return <div>Cargando...</div>;
+//   }
+
+//   // Define la plantilla Handlebars
+//   const templateSource = `   
+
+//     <div class="row">    
+//       <!-- Columna para la navegación -->
+//       <div class="col-md-6">
+//         <ul class="nav-list">
+//           {{#each navigation.[0].items}}
+//             <li class="nav-item">
+//               <a href="{{href}}" class="{{#if isCurrent}}current{{/if}}">{{text}}</a>
+//             </li>
+//           {{/each}}
+//         </ul>
+//       </div>
+
+//       <!-- Columna para el logo -->
+//       <div class="col-md-6">
+//         <a href="{{logo.[0].href}}">
+//           <img src="{{logo.[0].image.src}}" alt="{{logo.[0].alt}}" class="logo" />
+//         </a>
+//       </div>
+//     </div>  
+
+//     <div class="row">
+//       <!-- hotTopics -->
+//       <div class="col-lg-6">
+//         <nav class="navbar navbar-expand-lg navbar-light bg-light">
+//           <div class="navbar-nav">
+//             {{#each hotTopics}}
+//               <a class="nav-link" href="{{url}}" target="_blank">{{title}}</a>
+//             {{/each}}
+//           </div>
+//         </nav>
+//       </div>
+
+//       <!-- social -->
+//       <div class="col-lg-6">
+//         <div class="social">
+//           {{#each social.[0].items}}
+//             <a href="{{href}}" target="_blank">{{body}}</a>
+//           {{/each}}
+//         </div>
+//       </div>
+//     </div>
+
+//     <!-- pageLead -->
+//     <div class="page-lead">
+//       {{#each pageLead}}
+//         <img src="{{image.src}}" alt="{{alt}}" />
+//       {{/each}}
+//     </div>
+
+//     <!-- Breadcrumbs  -->
+//     <nav aria-label="breadcrumb">
+//       <ol class="breadcrumb">
+//         {{#each breadcrumbs}}
+//           <li class="breadcrumb-item"><a href="{{href}}">{{body}}</a></li>
+//         {{/each}}
+//       </ol>
+//     </nav>
+
+//     <div class="row">
+//       <!-- Columna izquierda para la imagen -->
+//       <div class="col-lg-6">
+//         <img src="{{lead.[0].image.src}}" alt="{{lead.[0].alt}}" class="lead-image" />
+//       </div>
+
+//       <!-- Columna derecha para el título y el párrafo -->
+//       <div class="col-lg-6">
+//         <h1>{{headline}}</h1>
+//         <p class="subheadline">{{subHeadline}}</p>
+//         <audio controls class="audio-player">
+//           <source src="{{audioPlayer.[0].audioUrl}}" type="audio/mp3">
+//           Tu navegador no soporta el elemento de audio.
+//         </audio>
+//         <!-- Actions -->
+//         <div class="actions">
+//           {{#each actions.[0].items}}
+//             <a href="{{shareHref}}" target="{{target}}">{{body}}</a>
+//           {{/each}}
+//         </div>
+//       </div>      
+    
+//   `;
+
+//   // Compila la plantilla Handlebars
+//   const template = Handlebars.compile(templateSource);
+
+//   // Renderiza la plantilla con los datos JSON
+//   const renderedTemplate = template(jsonData);
+
+//   return (
+//     <div dangerouslySetInnerHTML={{ __html: renderedTemplate }} />
+//   );
+// };
+
+// export default MyComponent;
+
 import React, { useState, useEffect } from 'react';
 import Handlebars from 'handlebars';
 import { fetchData } from './api';
@@ -22,25 +144,27 @@ const MyComponent = () => {
     return <div>Cargando...</div>;
   }
 
-  // Define la plantilla Handlebars
   const templateSource = `
-    <div class="mi-componente container">
-      <!-- navegacion -->
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="navbar-nav">
-          {{#each navigation.[0].items}}
-            <a class="nav-link" href="{{href}}" class="{{#if isCurrent}}current{{/if}}">{{text}}</a>
-          {{/each}}
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 order-md-1">
+          <ul class="nav-list">
+            {{#each navigation.[0].items}}
+              <li class="nav-item">
+                <a href="{{href}}" class="{{#if isCurrent}}current{{/if}}">{{text}}</a>
+              </li>
+            {{/each}}
+          </ul>
         </div>
-      </nav>
 
-      <!-- logo -->
-      <a href="{{logo.[0].href}}">
-        <img src="{{logo.[0].image.src}}" alt="{{logo.[0].alt}}" class="logo" />
-      </a>
+        <div class="col-md-6 order-md-2">
+          <a href="{{logo.[0].href}}">
+            <img src="{{logo.[0].image.src}}" alt="{{logo.[0].alt}}" class="img-fluid logo" />
+          </a>
+        </div>
+      </div>
 
       <div class="row">
-        <!-- hotTopics -->
         <div class="col-lg-6">
           <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="navbar-nav">
@@ -51,7 +175,6 @@ const MyComponent = () => {
           </nav>
         </div>
 
-        <!-- social -->
         <div class="col-lg-6">
           <div class="social">
             {{#each social.[0].items}}
@@ -61,14 +184,12 @@ const MyComponent = () => {
         </div>
       </div>
 
-      <!-- pageLead -->
       <div class="page-lead">
         {{#each pageLead}}
-          <img src="{{image.src}}" alt="{{alt}}" />
+          <img src="{{image.src}}" alt="{{alt}}" class="img-fluid w-100" />
         {{/each}}
       </div>
 
-      <!-- Breadcrumbs  -->
       <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
           {{#each breadcrumbs}}
@@ -78,20 +199,18 @@ const MyComponent = () => {
       </nav>
 
       <div class="row">
-        <!-- Columna izquierda para la imagen -->
         <div class="col-lg-6">
-          <img src="{{lead.[0].image.src}}" alt="{{lead.[0].alt}}" class="lead-image" />
+          <img src="{{lead.[0].image.src}}" alt="{{lead.[0].alt}}" class="img-fluid lead-image" />
         </div>
 
-        <!-- Columna derecha para el título y el párrafo -->
         <div class="col-lg-6">
-          <h1>{{headline}}</h1>
-          <p class="subheadline">{{subHeadline}}</p>
+          <h1 class="text-start">{{headline}}</h1>
+          <p class="text-start">{{subHeadline}}</p>
           <audio controls class="audio-player">
             <source src="{{audioPlayer.[0].audioUrl}}" type="audio/mp3">
             Tu navegador no soporta el elemento de audio.
           </audio>
-          <!-- Actions -->
+
           <div class="actions">
             {{#each actions.[0].items}}
               <a href="{{shareHref}}" target="{{target}}">{{body}}</a>
@@ -99,15 +218,10 @@ const MyComponent = () => {
           </div>
         </div>
       </div>
-
-      
     </div>
   `;
 
-  // Compila la plantilla Handlebars
   const template = Handlebars.compile(templateSource);
-
-  // Renderiza la plantilla con los datos JSON
   const renderedTemplate = template(jsonData);
 
   return (
